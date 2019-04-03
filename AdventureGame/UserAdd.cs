bd2ec1a3aec9b6f6199650d5e3f48b98c8716437
Marcin Zelkowski <zelkowski.m@interia.pl>
2019-04-03 18:05:40 +0200

    zajecia przed zaliczeniem 3

--- AdventureGame/UserAdd.cs
+++ AdventureGame/UserAdd.cs
@@ -12,9 +12,45 @@ namespace AdventureGame
 {
     public partial class UserAdd : Form
     {
+        int x = 1;
         public UserAdd()
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("user" + x);
+            x++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+                if (MessageBox.Show("Czy chcesz usunąć użytkownika " +
+                     listBox1.GetItemText(listBox1.SelectedItem), "Uwaga", MessageBoxButtons.YesNo,
+                     MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                }
+        }
+
+        //using przy używaniu streamódo pliku 
+    //    try{
+    //    File.WriteAllText("plik","text",Encoding,Default);
+    //        catch (InvalidOperationException err){//blad}
+
+    //}
+    //if(FileDialog.exist("plik"))
+    //try{
+    //    File.AppendAllLine("plik","text",Encoding,Default);
+    //        catch (InvalidOperationException err){//blad}
+
+    //}  konwertować do base64(tobase64string) i to do pliku  zamiast od ascii
+
+
+
+
+
     }
 }
