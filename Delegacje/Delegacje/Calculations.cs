a068d0ea985619fc1403596d1c30a3820f911ebe
Marcin Zelkowski <zelkowski.m@interia.pl>
2019-04-10 17:45:42 +0200

    zajęcia delegaty

---
+++ Delegacje/Delegacje/Calculations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacje
+{
+    class Calculations
+    {
+        public delegate int Oper(int a, int b);
+        public Oper o;
+        public Calculations()
+        {
+            o += Add;
+            o += Substract;
+            o -=Substract;
+        }
+        private int Add(int x,int b)
+        {
+            return x + b;
+        }
+        private int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Delegacje/Delegacje/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegacje
+{
+    public partial class Form1 : Form
+    {
+        delegate int SetMe(int a, int b);
+        delegate int Set2(string u, string p);
+        //int auth(string login, string password);
+        delegate int Set3();
+        //int loaduser();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private int Dodaj(int x,int y)
+        {
+            return x + y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetMe s = new SetMe(Dodaj);
+            int wynik = s.Invoke(2, 3);
+            MessageBox.Show("Wynik " + wynik);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculations i = new Calculations();
+            int a=i.o.Invoke(4, 8);
+            MessageBox.Show("wynik " + a);
+        }
+    }
+}
